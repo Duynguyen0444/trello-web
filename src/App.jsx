ab0c804a1d14f6fe1ad33fc3a8259706298fede9
Duynguyen <duynguyen0444@gmail.com>
2025-07-15 22:13:31 +0700

    Enhance App component structure and theming support

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Button from '@mui/material/Button'
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <>
+      <div>asdas</div>
+      <Typography variant='body2' color='text.secondary'>asodin</Typography>
       <Button variant='text'>Text</Button>
       <Button variant='contained'>Contained</Button>
       <Button variant='outlined'>Outlined</Button>
