c6a2b291d71e8b1115308c9377f0ee06a7624027
Duynguyen <duynguyen0444@gmail.com>
2025-08-11 23:16:52 +0700

    Refactor project

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
-import Button from '@mui/material/Button'
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
-import Typography from '@mui/material/Typography'
-
-import {
-  experimental_extendTheme as extendTheme,
-  useColorScheme,
-} from '@mui/material/styles';
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme();
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light');
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  );
-}
+import Board from './pages/Boards/_id'
 
 function App() {
   return (
-    <>
-      <ModeToggle />
-      <hr />
-      <div>asdas</div>
-      <Typography variant='body2' color='text.secondary'>asodin</Typography>
-      <Button variant='text'>Text</Button>
-      <Button variant='contained'>Contained</Button>
-      <Button variant='outlined'>Outlined</Button>
-      <br />
-      <AccessAlarm />
-      <ThreeDRotation />
-    </>
+    <Board />
   )
 }
 
